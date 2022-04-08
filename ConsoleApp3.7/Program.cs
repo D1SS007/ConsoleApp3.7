657e470e854d06560f38b05134699518b673d07e
D1SS007 <101556566+D1SS007@users.noreply.github.com>
2022-04-08 16:20:22 +0300

    Update Program.cs

--- ConsoleApp3.7/Program.cs
+++ ConsoleApp3.7/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp3._7
         {
             string line = Console.ReadLine();
             string[] parts = line.Split(); 
+            
             for (int i = 0; i < parts.Length; i++)
             {
                 Console.WriteLine(parts[i]);
