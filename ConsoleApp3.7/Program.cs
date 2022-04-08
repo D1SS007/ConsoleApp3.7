bbd8a82668dff58c02639a1267e69f186c131fe7
Alex Zverev <lordzver007@yandex.ru>
2022-04-08 15:51:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3.7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] parts = line.Split(); 
+            for (int i = 0; i < parts.Length; i++)
+            {
+                Console.WriteLine(parts[i]);
+            }  
+        }
+    }
+}
